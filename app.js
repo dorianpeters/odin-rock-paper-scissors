4b1ed21c0621e56f4df23c7eb985caa206ba8b87
Dorian Peters <dpeters08@gmail.com>
2024-01-13 23:44:15 -0800

    Draft psuedo code in app.js listing proposed algorithm for this game.

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log("Hello, World")
+
+/*
+# create function getComputerChoice
+  generate random number between 1-3
+    If 1 - "Rock", If 2-"Paper", If 3 - Scissors
+    Store computerChoice
+    Return computerChoice
+
+# create function getPlayChoice
+    Prompt user to type choice
+    store choice in variable
+    lower case choice for comparison
+    if choice = rock, paper, or scissors
+      return choice
+    else
+      tell user, invalid choice
+      reprompt/start over
+
+# create function playRound
+  Take two string parametors - player selection and computer selection
+  Start with: if playerSelection = rock AND computerselection = rock, answer = tie
+              if playerSelection = rock AND computerselection = paper, answer = computer wins
+              if playerSelection = rock AND computerselection = scissors, answer = player wins
+              (same thing with other combinations)
+  Returns string that declares the winner.
+
+  # create function game() that keeps score and does a best of five
+      create variables for playerScore and computerScore.
+      playRound
+      if player wins, increase score
+      if computer wins, increase score
+      Stop when playerScore or ComputerScore reach three.
+*/
+  
