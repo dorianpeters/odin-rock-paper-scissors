6b1ff317b71a9bc00ed7e9530817170bd011eb70
Dorian Peters <dpeters08@gmail.com>
2024-01-14 20:25:33 -0800

    Make background darker, write getComputerChoice function

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 console.log("Hello, World")
+for (let i = 0; i < 30; i++) {
+  console.log(getComputerChoice())
+}
 
 /*
 # create function getComputerChoice
   generate random number between 1-3
     If 1 - "Rock", If 2-"Paper", If 3 - Scissors
     Store computerChoice
-    Return computerChoice
+    Return computerChoice */
 
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3 + 1)
+  if (choice === 1) {
+    return "Rock";
+  }  else if (choice === 2) {
+    return "Paper"
+  }  else if (choice === 3) {
+    return "Scissors"
+  } else {
+    console.log("Error: Random number outside 1-3");
+  }
+}
+
+/*
 # create function getPlayChoice
     Prompt user to type choice
     store choice in variable
